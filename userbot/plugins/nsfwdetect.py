3cf8b778db5c64588bc9000c9f566b68575dcf3c
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-25 20:22:25 +0530

    Update nsfwdetect.py

--- userbot/plugins/nsfwdetect.py
+++ userbot/plugins/nsfwdetect.py
@@ -6,8 +6,8 @@ import os
 import requests
 
 
-@bot.on(admin_cmd(pattern="detect$", outgoing=True))
-@bot.on(sudo_cmd(pattern="detect$", allow_sudo=True))
+@borg.on(admin_cmd(pattern="detect$", outgoing=True))
+@borg.on(sudo_cmd(pattern="detect$", allow_sudo=True))
 async def detect(event):
     if Config.DEEP_AI is None:
         return await edit_delete(
