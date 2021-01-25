196e4cfc880d9798047cc5a57857f518e7b48f33
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-26 03:35:34 +0530

    Update geeta.py

--- userbot/plugins/geeta.py
+++ userbot/plugins/geeta.py
@@ -46,7 +46,7 @@ async def gita(event):
 CMD_HELP.update(
     {
         "gita":".verse <chapter_number> <verse_number> "
-        "\nUsage":"Get a specific verse from a particular chapter \n\n"
+        "\nUsage: Get a specific verse from a particular chapter \n\n"
         "gita":".verse <chapter_number> <verse_number> hi "
-        "\nUsage":"Get a specific verse from a particular chapter in hindi.\n\n"
+        "\nUsage: Get a specific verse from a particular chapter in hindi.\\n\n"
     }
