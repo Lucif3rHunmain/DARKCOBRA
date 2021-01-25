766c9782f0378db3d84fec0acefa82192f98408b
Lucif3rHunmain <77950911+Lucif3rHunmain@users.noreply.github.com>
2021-01-25 13:17:28 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -12,7 +12,7 @@ from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
 from userbot import CMD_HELP, bot
 from userbot.utils import admin_cmd, sudo_cmd
 
-UPSTREAM_REPO_URL = "https://github.com/Lucif3rHunmain/DARKCOBRA/"
+UPSTREAM_REPO_URL = "https://github.com/Lucif3rHunmain/DARKCOBR/"
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
 
